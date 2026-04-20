2bde543c30db405674a5a58f20e937baaeec31bb
yurabiryukov <smile19x@yandex.ru>
2026-04-20 17:38:08 +1000

    Minor fixes

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -4,8 +4,6 @@ from pages.registration_page import RegistrationPage
 
 def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
     registration_page.visit(url='https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
     registration_page.fill_registration_form(email='user.name@gmail.com', username='username', password='password')
     registration_page.click_registration_button()
-
     dashboard_page.check_visible_text()
